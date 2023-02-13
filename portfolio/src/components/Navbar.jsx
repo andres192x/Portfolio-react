6d847d83e35215529ca47038e8639c17c5abfdf3
Andres Chaux <git@github.com:andres192x>
2023-02-13 14:19:03 -0500

    added instructions in the readme, modified components

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
         </Link>
         <nav className="md:mr-auto md:ml-4 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center">
           <Link className="mr-5 hover:text.white" to={"/projects"}>Projects</Link>
-          <Link className="mr-5 hover:text.white" to={"/projects"}>Skills</Link>
+          {/* <Link className="mr-5 hover:text.white" to={"/projects"}>Skills</Link> */}
           <Link className="mr-5 hover:text.white" to={"/contact"}>Contact</Link>
         </nav>
         {/* <Link className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0" to={"/contact"}>
